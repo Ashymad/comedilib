c83851d46f26dae42171c848adfcc08e5607fdb8
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2021-01-16 19:53:45 +0100

    to_phys and friends

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ use comedilib_sys as ffi;
 use std::ffi::{CString, CStr};
 use std::os::raw::*;
 use failure::{bail, Error};
-use num_enum::IntoPrimitive;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::convert::TryFrom;
 
 macro_rules! perror {
     ($str:expr) => {
@@ -10,7 +11,8 @@ macro_rules! perror {
     }
 }
 
-type LSampl = ffi::lsampl_t;
+pub type LSampl = ffi::lsampl_t;
+pub type Range = ffi::comedi_range;
 
 #[derive(IntoPrimitive)]
 #[repr(u32)]
@@ -20,6 +22,14 @@ pub enum AREF {
     DIFF = ffi::AREF_DIFF,
     OTHER = ffi::AREF_OTHER
 }
+
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u32)]
+pub enum OORBehavior {
+    NAN = ffi::comedi_oor_behavior_COMEDI_OOR_NAN,
+    NUMBER = ffi::comedi_oor_behavior_COMEDI_OOR_NUMBER,
+}
+
 
 pub struct Comedi {
     ptr: *mut ffi::comedi_t,
@@ -32,15 +42,41 @@ impl Comedi {
         if ptr.is_null() {
             perror!(format!("comedi_open({})", dev.into_string().unwrap()));
         }
-        return Ok(Comedi { ptr });
+        Ok(Comedi { ptr })
     }
     pub fn data_read(&self, subdevice: c_uint, channel: c_uint, range: c_uint, aref: AREF) -> Result<LSampl, Error> {
         let mut data: LSampl = 0;
         if unsafe { ffi::comedi_data_read(self.ptr, subdevice, channel, range, aref.into(), &mut data) } < 0 {
             perror!("comedi_data_read");
         }
-        return Ok(data);
+        Ok(data)
     }
+    pub fn get_range<'a>(&'a self, subdevice: c_uint, channel: c_uint, range: c_uint) -> Result<&'a mut Range, Error> {
+        let range = unsafe { ffi::comedi_get_range(self.ptr, subdevice, channel, range) };
+        if range.is_null() {
+            perror!("comedi_get_range");
+        }
+        Ok(unsafe { &mut*range })
+    }
+    pub fn get_maxdata(&self, subdevice: c_uint, channel: c_uint) -> Result<LSampl,Error> {
+        let maxdata = unsafe { ffi::comedi_get_maxdata(self.ptr, subdevice, channel) };
+        if maxdata == 0 {
+            perror!("comedi_get_maxval");
+        }
+        Ok(maxdata)
+    }
+}
+
+pub fn to_phys(data: LSampl, range: &mut Range, maxdata: LSampl) -> Result<c_double,Error> {
+    let phys = unsafe { ffi::comedi_to_phys(data, range as *mut Range, maxdata) };
+    if phys == c_double::NAN {
+        perror!("comedi_to_phys");
+    }
+    Ok(phys)
+}
+
+pub fn set_global_oor_behavior(behavior: OORBehavior) -> OORBehavior {
+    OORBehavior::try_from(unsafe { ffi::comedi_set_global_oor_behavior(behavior.into()) }).unwrap()
 }
 
 impl Drop for Comedi {
--- src/test.rs
+++ src/test.rs
@@ -3,5 +3,23 @@ use super::*;
 #[test]
 fn open_device_and_read_data() {
     let dev = Comedi::open(0).unwrap();
+    let _sample = dev.data_read(0, 0, 0, AREF::GROUND).unwrap();
+}
+
+#[test]
+fn range_lifetime_test() {
+    //let range = { // shouldn't compile
+    let dev = Comedi::open(0).unwrap();
+    dev.get_range(0, 0, 0).unwrap();
+    //};
+}
+
+#[test]
+fn convert_to_phys() {
+    let dev = Comedi::open(0).unwrap();
+    set_global_oor_behavior(OORBehavior::NAN);
+    let range = dev.get_range(0, 0, 0).unwrap();
+    let maxdata = dev.get_maxdata(0, 0).unwrap();
     let sample = dev.data_read(0, 0, 0, AREF::GROUND).unwrap();
+    to_phys(sample, range, maxdata).expect("Out of range value");
 }
